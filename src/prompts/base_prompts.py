1aca47c02805d09de1f12d8d95e416bc594cda6e
saehoon0501 <saehoon0501@gmail.com>
2025-08-01 15:02:36 +0900

    base_prompt field updated

--- src/prompts/base_prompts.py
+++ src/prompts/base_prompts.py
@@ -3,9 +3,8 @@ from langchain_core.prompts import ChatPromptTemplate
 from pydantic import BaseModel
 
 class BasePrompt(BaseModel):
-    model_name: str
-    date_created: datetime
-    prompt_test: str
+    """Base prompt for all prompts."""
+    date_created: datetime    
     description: str
     creator: str
     prompt: ChatPromptTemplate
